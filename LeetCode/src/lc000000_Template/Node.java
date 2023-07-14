a1305495ca04f9ef970a7911ff13a09e6c9f5a0a
user_123 <11405033+user-123@users.noreply.github.com>
2023-07-15 03:05:28 +0800

    update LeetCode/lc000000_Template (2023.07.15 03:05)

---
+++ LeetCode/src/lc000000_Template/Node.java
@@ -0,0 +1,27 @@
+package lc000000_Template;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Node {
+	public int val;
+	public List<Node> neighbors;
+	public Node() {
+		val = 0;
+		neighbors = new ArrayList<Node>();
+	}
+	public Node(int _val) {
+		val = _val;
+		neighbors = new ArrayList<Node>();
+	}
+	public Node(int _val, ArrayList<Node> _neighbors) {
+		val = _val;
+		neighbors = _neighbors;
+	}
+
+	@Override
+	public String toString() {
+		return "Node [val=" + val + ", neighbors=" + neighbors + "]";
+	}
+
+}
