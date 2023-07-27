c137384642620f417335b0f51ba0fcf1cda3e654
user_123 <11405033+user-123@users.noreply.github.com>
2023-07-28 04:19:19 +0800

    update LeetCode/lc002141_MaximumRunningTimeOfNComputers (2023.07.28 04:19)

--- LeetCode/src/lc002141_MaximumRunningTimeOfNComputers/Solution.java
+++ LeetCode/src/lc002141_MaximumRunningTimeOfNComputers/Solution.java
@@ -7,7 +7,7 @@ public class Solution {
 	public Solution() {}
 
 	public long maxRunTime(int n, int[] batteries) {
-		//思路：假設能完美平均的使用並耗盡每顆電池，則能得到最長時間；將電池按電量排序，依反序檢查，若是電池能讓一台電腦持續運行到maxTime，則繼續檢查，直到遇到無法達成的電池，則它的使用時間就是maxTime
+		//思路：假設能完美平均的使用並耗盡每顆電池，則能得到最長時間；將電池按電量排序，依反序檢查，若是電池能讓一台電腦持續運行超過maxTime，則繼續檢查，直到遇到無法達成的電池，則它的使用時間就是maxTime
 		Arrays.sort(batteries);
 		int batteryQuantity=batteries.length;
 		if(batteryQuantity==n) {
@@ -19,7 +19,7 @@ public class Solution {
 		}
 		maxTime=maxEnergy/n;
 		int computerCount=0;
-		while(batteries[batteryQuantity-1-computerCount]>maxTime) {
+		while(batteries[batteryQuantity-1-computerCount]>maxTime) {	//這裡不計算batteries[batteryQuantity-1-computerCount]==maxTime的原因有兩個：一、不影響maxTime(不會變小)，因為沒有多餘沒用的電量，二、甚至會在會計算到後面的時候因為多餘的電量，而使計算的maxTime變大(我們已知最長的時間就是初始的maxTime)(舉例；n=3, batteries={1, 3, 3, 3})
 			maxEnergy-=batteries[batteryQuantity-1-computerCount++];
 			maxTime=maxEnergy/(n-computerCount);
 		}
